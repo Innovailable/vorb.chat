6a9d9c6128dac014a7b2bdd14de35c32f39ee722
Thammi <thammi@chaossource.net>
2020-05-14 18:28:14 +0200

    Add random default names

--- client/components/start_page.tsx
+++ client/components/start_page.tsx
@@ -1,24 +1,31 @@
 import * as React from 'react';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 
 import {useHistory } from 'react-router-dom';
-
 import { FeatherIcon } from './feather';
+
+import * as Readable from 'readable-url-names';
 
 import { Button } from '@rmwc/button';
 import { TextField } from '@rmwc/textfield';
+import { Footer } from './footer';
 
 export const StartPage: React.SFC = () => {
-  const [roomName, setRoomName] = useState("NBA");
+  const [roomName, setRoomName] = useState("");
   const history = useHistory();
 
+  const defaultName = useMemo(() => {
+    const readable = new Readable(true, 5);
+    return readable.generate();
+  }, []);
 
   const updateRoom = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setRoomName(e.target.value);
   }, [setRoomName]);
 
   const joinRoom = useCallback(() => {
-    history.push(`/c/${roomName}`)
+    const room = roomName || defaultName;
+    history.push(`/c/${room}`)
   },[roomName]);
 
   const onKeyDown = useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
